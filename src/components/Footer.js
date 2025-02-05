0d2e2300475f0083cb80736412c1a153d1e43726
rithvik23-root <rithvikarelli7@gmail.com>
2025-02-05 16:54:43 -0600

    changed zoom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,15 @@ export default function FooterComponent() {
   return (
     <div className="footer">
       <div className="media">
-        <a
-          href="https://github.com/rithvik23-root"
-          alt="git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="/git.jpg" className="git_img" alt="git"></img>
-        </a>
+
+          
+          <a href="https://github.com/rithvik23-root" rel="noreferrer" target="_blank"><img src="/github.png" className="git_img" alt="git"/></a>
+          <a href="https://www.instagram.com/_rithvik23_/" rel="noreferrer" target="_blank"><img src="/instagram.png" className="git_img" alt="git"/></a>
+          <a href="https://www.linkedin.com/in/rithvikarelli" rel="noreferrer" target="_blank"><img src="/linkedin.png" className="git_img" alt="git"/></a>
+
+        
       </div>
-      <p className="namef">rithvik</p>
+      <p className="namef">rithvik ©2025</p>
     </div>
   );
 }
